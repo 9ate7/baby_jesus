dc552a0873606d99e4c11ad87609d6f9fae35d09
rsshelas <rsshelas@gmail.com>
2014-09-01 01:47:21 -0400

    Added Hello World

---
+++ Saleh/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * @(#)Text1.java
+ *
+ *
+ * @author 
+ * @version 1.00 2014/9/1
+ */
+
+
+public class HelloWorld { 
+   public static void main(String[] args) { 
+      System.out.println("Hello World!");
+      
+   }
+}
